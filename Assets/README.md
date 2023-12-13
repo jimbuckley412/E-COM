10a8abfd1e5a3b3c96ee6cd3f71bd01ab8e247e6
jimbuckley412 <jimbuckley412@gmail.com>
2023-12-13 11:46:23 -0500

    E-COM

---
+++ Assets/README.md
@@ -0,0 +1,69 @@
+# E-Comm
+
+E-Comm is a simple e-commerce application that allows users to browse products, add them to a cart, and checkout. The application is built using React, Redux, and Firebase. The application is deployed using Heroku and can be accessed [here](https://e-comm-2021.herokuapp.com/).
+
+## Table of Contents
+
+- [Project Name](#project-name)
+  - [Table of Contents](#table-of-contents)
+  - [Description](#description)
+  - [Technologies Used](#technologies-used)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [License](#license)
+  - [Questions](#questions)
+
+## Description
+
+E-Comm is a simple e-commerce application that allows users to browse products, add them to a cart, and checkout. The application is built using React, Redux, and Firebase. The application is deployed using Heroku and can be accessed [here](https://e-comm-2021.herokuapp.com/).
+
+## Technologies Used
+
+List the key technologies and tools used in your project.
+
+- [React](https://reactjs.org/)
+- [Redux](https://redux.js.org/)
+- [Firebase](https://firebase.google.com/)
+- [Heroku](https://www.heroku.com/)
+- [Stripe](https://stripe.com/)
+- [SASS](https://sass-lang.com/)
+- [Bootstrap](https://getbootstrap.com/)
+- [Axios](https://www.npmjs.com/package/axios)
+- [Redux Persist](https://www.npmjs.com/package/redux-persist)
+- [React Router](https://reactrouter.com/)
+- [React Stripe Checkout](https://www.npmjs.com/package/react-stripe-checkout)
+
+## Installation
+
+Provide instructions on how to install your project. Include any dependencies that need to be installed and steps to set up the project.
+
+```bash
+# Clone the repository
+git clone https://github.com/your-username/your-repository.git
+
+# Navigate to the project directory
+cd your-repository
+
+# Install dependencies
+npm install
+
+# Start the development server
+npm start
+```
+
+## Usage
+
+Provide instructions and examples for the use of your project. Include screenshots as needed.
+
+## Contributing
+
+Provide instructions on how other developers can contribute to your project. Include information about your coding standards, and the process for submitting pull requests.
+
+## License
+
+The MIT License (MIT)
+
+## Questions
+
+If you have any questions about the project, open an issue or contact me directly at [email](mailto:jimbuckley412@gmail.com). You can find more of my work at [GitHub](
---
+++ Assets/routes/api/product-routes.js
@@ -0,0 +1,129 @@
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag } = require('../../models');
+
+// The `/api/products` endpoint
+
+// get all products
+router.get('/', async (req, res) => {
+  // find all products
+  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one product
+router.get('/:id', async (req, res) => {
+  // find a single product by its `id`
+  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create new product
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData);
+
+    // If there are tagIds, create ProductTag entries
+    if (req.body.tagIds && req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: productData.id,
+          tag_id,
+        };
+      });
+      await ProductTag.bulkCreate(productTagIdArr);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+// update product
+router.put('/:id', async (req, res) => {
+  // update product data
+  try {
+    const product = await Product.findByPk(req.params.id);
+
+    if (!product) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+
+    await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    // If there are tagIds, update ProductTag entries
+    if (req.body.tagIds && req.body.tagIds.length) {
+      const productTags = await ProductTag.findAll({
+        where: { product_id: req.params.id },
+      });
+
+      // Create a filtered list of new tag_ids
+      const productTagIds = productTags.map(({ tag_id }) => tag_id);
+      const newProductTags = req.body.tagIds
+        .filter((tag_id) => !productTagIds.includes(tag_id))
+        .map((tag_id) => {
+          return {
+            product_id: req.params.id,
+            tag_id,
+          };
+        });
+
+      // Figure out which ones to remove
+      const productTagsToRemove = productTags
+        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .map(({ id }) => id);
+
+      // Run both actions
+      await Promise.all([
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
+      ]);
+    }
+
+    res.json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: { id: req.params.id },
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
